2225db9123273829f615f746a23f3e7991b7d516
Arvin Maltezo Bareng <arvin.maltezo.bareng@gmail.com>
2018-05-29 13:36:18 +0800

    Made Shooter Follow Finger

---
+++ Space Shooter/Assets/Scripts/TouchMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TouchMovement : MonoBehaviour {
+	public Vector3 pos;
+
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android) { // Checks if running at android or not
+			pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.GetTouch (0).position.x, Input.GetTouch (0).position.y, 1));
+		} else {
+			pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 1));
+		}
+
+		transform.position = new Vector3 (pos.x, pos.y + 0.5f, pos.z);
+	}
+}
