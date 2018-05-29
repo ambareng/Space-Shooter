8d1ac68f675276bd5294bcb8095460f4ecbca6f8
Arvin Maltezo Bareng <arvin.maltezo.bareng@gmail.com>
2018-05-29 14:56:01 +0800

    Make Shooter Shoot Bullets

---
+++ Space Shooter/Assets/Scripts/BulletController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+	public float bulletSpeed;
+	private Renderer compRenderer;
+
+	void Awake () {
+		bulletSpeed = 20f;
+		compRenderer = this.gameObject.GetComponent<Renderer> ();
+	}
+
+	void Update () {
+		transform.Translate (0f, bulletSpeed * Time.deltaTime, 0f);
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		Destroy (this.gameObject);
+	}
+}
---
+++ Space Shooter/Assets/Scripts/ShootController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootController : MonoBehaviour {
+	public float shootCooldown;
+	public GameObject bullet;
+	Vector3 shootPos;
+
+	void Update () {
+		shootCooldown -= Time.deltaTime;
+		shootPos = transform.position;
+
+		if (shootCooldown <= 0) {
+			StartCoroutine ("Shoot");
+			shootCooldown = 0.25f;
+		}
+	}
+
+	IEnumerator Shoot () {
+		GameObject bulletObj = Instantiate (bullet, shootPos, Quaternion.identity);
+
+		bulletObj.transform.position = shootPos;
+		yield return new WaitForSeconds (0.0000001f);
+	}
+}
